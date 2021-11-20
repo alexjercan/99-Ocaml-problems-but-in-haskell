705d17e61c439a989e03c72074da631536ddc861
Alexandru Jercan <jercan_alex27@yahoo.com>
2021-11-20 18:33:40 +0200

    feat: added problem 17

--- main.hs
+++ main.hs
@@ -1,5 +1,5 @@
 module Main where
-import Data.List (group)
+import Data.List (group, intercalate)
 
 -- Problem 01
 myLast :: [a] -> Maybe a
@@ -74,6 +74,63 @@ myEncode' :: Eq a => [a] -> [(Int, a)]
 myEncode' xs = zipWith f gs gs
     where gs = group xs
           f = flip ((,) . length) . head
+
+-- Problem 11
+data RList a = ConsOne a (RList a)
+             | ConsMany (Int, a) (RList a)
+             | REmpty
+
+instance Show a => Show (RList a) where
+    show ys = "[" ++ intercalate "," (go ys) ++ "]"
+        where go REmpty = []
+              go (ConsOne x xs) = show x : go xs
+              go (ConsMany x xs) = show x : go xs
+
+myEncodeR :: Eq a => [a] -> RList a
+myEncodeR = f . group
+    where f [] = REmpty
+          f (x:xs)
+            | length x == 1 = ConsOne (head x) (f xs)
+            | otherwise     = ConsMany (length x, head x) (f xs)
+
+-- Problem 12
+myDecodeR :: RList a -> [a]
+myDecodeR REmpty = []
+myDecodeR (ConsOne x xs) = x : myDecodeR xs
+myDecodeR (ConsMany (l, x) xs) = replicate l x ++ myDecodeR xs
+
+-- Problem 13
+myEncodeRD :: Eq a => [a] -> RList a
+myEncodeRD = go 1
+    where go _ [] = REmpty
+          go 1 [x] = ConsOne x REmpty
+          go acc [x] = ConsMany (acc, x) REmpty
+          go acc (x:xs@(y:_))
+            | x /= y && acc == 1 = ConsOne x (go 1 xs)
+            | x /= y             = ConsMany (acc, x) (go 1 xs)
+            | otherwise = go (acc+1) xs
+
+-- Problem 14
+myDuplicate :: [a] -> [a]
+myDuplicate [] = []
+myDuplicate (x:xs) = x : x : myDuplicate xs
+
+-- Problem 15
+myReplicate :: [a] -> Int -> [a]
+myReplicate [] _ = []
+myReplicate (x:xs) n = replicate n x ++ myReplicate xs n
+
+-- Problem 16
+myDrop :: [a] -> Int -> [a]
+myDrop = go 1
+    where go _ [] _ = []
+          go acc (x:xs) n
+            | n == acc  = go 1 xs n
+            | otherwise = x : go (acc+1) xs n
+
+-- Problem 17
+mySplit :: [a] -> Int -> ([a], [a])
+mySplit xs n = splitAt n xs
 
 main :: IO ()
 main = undefined
