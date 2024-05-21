a6d3823346ba6b9611137298ebb829c5e4db3f15
Alex Jercan <jercan_alex27@yahoo.com>
2024-05-21 07:39:11 +0300

    more things added

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,10 @@
 module Main where
 
 import Prelude hiding (last, length, reverse)
+import System.Random (uniformR, mkStdGen, RandomGen)
+import Data.List (unfoldr)
+import Data.Time (getCurrentTime, UTCTime (utctDayTime))
+import Data.Time.Clock (diffTimeToPicoseconds)
 
 last :: [a] -> Maybe a
 last [] = Nothing
@@ -76,8 +80,53 @@ drop' :: [a] -> Int -> [a]
 drop' [] _ = []
 drop' xs n = take (n - 1) xs ++ drop' (drop n xs) n
 
+split :: [a] -> Int -> ([a], [a])
+split xs n = (take n xs, drop n xs)
+
+slice :: [a] -> Int -> Int -> [a]
+slice xs i k = take (k - i + 1) $ drop i xs
+
+rotate :: [a] -> Int -> [a]
+rotate xs n = b ++ a
+    where (a, b) = split xs n
+
+removeAt :: Int -> [a] -> [a]
+removeAt k xs = (take k xs) ++ (drop (k + 1) xs)
+
+insertAt :: a -> Int -> [a] -> [a]
+insertAt e n xs = a ++ [e] ++ b
+    where (a, b) = split xs n
+
+range :: Int -> Int -> [Int]
+range a b = if a > b then dec a b else inc a b
+    where
+        dec a' b' = if a' < b' then [] else a' : dec (a' - 1) b'
+        inc a' b' = if a' > b' then [] else a' : inc (a' + 1) b'
+
+randSelect :: Int -> [a] -> Int -> [a]
+randSelect seed xs n = take n $ unfoldr go (mkStdGen seed, xs)
+    where
+        go :: RandomGen g => (g, [a]) -> Maybe (a, (g, [a]))
+        go (_, []) = Nothing
+        go (g, ys) = let (i, g') = uniformR (0, length ys - 1) g in Just (ys !! i, (g', removeAt i ys))
+
+lottoSelect :: Int -> Int -> Int -> [Int]
+lottoSelect seed k n = randSelect seed (range 1 n) k
+
+permutation :: Int -> [a] -> [a]
+permutation seed xs = randSelect seed xs $ length xs
+
+extract :: Int -> [a] -> [[a]]
+extract k _ | k <= 0 = [[]]
+extract _ [] = []
+extract k (x:xs) = let withX = map (x:) $ extract (k - 1) xs
+                       withoutX = extract k xs
+                   in withX ++ withoutX
+
 main :: IO ()
 main = do
+    time <- (fromInteger . diffTimeToPicoseconds . utctDayTime) <$> getCurrentTime
+
     putStrLn "# Tail of a List"
     print (last ["a", "b", "c"])
     print ((last :: [Int] -> Maybe Int) [])
@@ -116,3 +165,25 @@ main = do
     print (replicateList ["a", "b", "c", "c", "d"] 3)
     putStrLn "# Drop Every N'th Element From a List"
     print (drop' ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"] 3)
+    putStrLn "# Split a List Into Two Parts; The Length of the First Part Is Given"
+    print (split ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"] 3)
+    print (split ["a", "b", "c", "d"] 5)
+    putStrLn "# Extract a Slice From a List"
+    print (slice ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"] 2 6)
+    putStrLn "# Rotate a List N Places to the Left"
+    print (rotate ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"] 3)
+    putStrLn "# Remove the K'th Element From a List"
+    print (removeAt 1 ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"])
+    putStrLn "# Insert an Element at a Given Position Into a List"
+    print (insertAt "alpha" 1 ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"])
+    putStrLn "# Create a List Containing All Integers Within a Given Range"
+    print (range 4 9)
+    print (range 9 4)
+    putStrLn "# Extract a Given Number of Randomly Selected Elements From a List"
+    print (randSelect time ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"] 3)
+    putStrLn "# Lotto: Draw N Different Random Numbers From the Set 1..M"
+    print (lottoSelect time 6 49)
+    putStrLn "# Generate a Random Permutation of the Elements of a List"
+    print (permutation time ["a", "b", "c", "d", "e", "f"])
+    putStrLn "# Generate the Combinations of K Distinct Objects Chosen From the N Elements of a List"
+    print (extract 2 ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"])
